7a13affba0e7d1bb1a7487c88249aa2ed2225757
if1live <libsora25@gmail.com>
2023-12-29 21:55:38 +0900

    engine.io example

---
+++ examples/plain/client_simple.js
@@ -0,0 +1,92 @@
+import * as eio from "engine.io-client";
+import { setTimeout } from "timers/promises";
+
+/*
+https://github.com/socketio/engine.io-client
+예제에서는 engine.io-client 의 메세지를 보고 싶어서 처리할수 있는 대부분을 넣었다.
+webscoekt만 쓸거라서 upgrade는 안넣었다
+*/
+
+const host = process.env.WS_URL ?? "ws://127.0.0.1:3001";
+const socket = new eio.Socket(host, {
+  path: "/",
+  transports: ["websocket"],
+});
+socket.binaryType = "arraybuffer";
+
+/** Fired upon successful connection. */
+socket.on("open", async () => {
+  // 핸들러 등록 이후에 원하는 작업을 시작해야한다.
+  // 메세지 전송과 message 핸들러 등록의 순서를 바꾸면 응답을 못받을 수 있다.
+  console.log(`open: ${new Date().toISOString()}`);
+
+  {
+    socket.send("message-text");
+    await setTimeout(10);
+  }
+
+  {
+    const arr = new Uint8Array(2);
+    arr[0] = 0x12;
+    arr[1] = 0x34;
+    socket.send(arr);
+    await setTimeout(10);
+  }
+
+  await setTimeout(60_000);
+  socket.close();
+});
+
+/**
+ * Fired when data is received from the server.
+ * Arguments
+ * String | ArrayBuffer: utf-8 encoded data or ArrayBuffer containing binary data
+ */
+socket.on("message", (data) => {
+  if (typeof data === "string") {
+    console.log(`message_string: ${new Date().toISOString()} ${data}`);
+  } else if (data instanceof ArrayBuffer) {
+    let line = "";
+    for (const byte of new Uint8Array(data)) {
+      const hex = byte.toString(16).padStart(2, "0");
+      line += `0x${hex} `;
+    }
+    console.log(`message_arraybuffer: ${new Date().toISOString()} ${line}`);
+  } else {
+    console.log(`message_unknown: ${new Date().toISOString()} ${data}`);
+  }
+});
+
+/**
+ * Fired upon disconnection.
+ * In compliance with the WebSocket API spec, this event may be fired even if the open event does not occur
+ * (i.e. due to connection error or close()).
+ */
+socket.on("close", () => {
+  console.log(`close: ${new Date().toISOString()}`);
+});
+
+/** Fired when an error occurs. */
+socket.on("error", (err) => {
+  console.log(`error: ${new Date().toISOString()} ${err}`);
+});
+
+/** Fired upon completing a buffer flush */
+socket.on("flush", () => {
+  console.log(`flush: ${new Date().toISOString()}`);
+});
+
+/** Fired after drain event of transport if writeBuffer is empty */
+socket.on("drain", () => {
+  console.log(`drain: ${new Date().toISOString()}`);
+});
+
+/** Fired upon receiving a ping packet. */
+socket.on("ping", () => {
+  console.log(`ping: ${new Date().toISOString()}`);
+});
+
+/** Fired upon flushing a pong packet (ie: actual packet write out) */
+socket.on("pong", (x) => {
+  console.log(`pong: ${new Date().toISOString()}`);
+});
---
+++ examples/plain/server.ts
@@ -0,0 +1,81 @@
+import http from "node:http";
+import { setTimeout } from "node:timers/promises";
+import * as eio from "engine.io";
+
+/*
+https://github.com/socketio/engine.io
+공식 문서에 정의된 이벤트 대부분 넣어봄
+*/
+
+const server = http.createServer();
+const io = eio.attach(server, {
+  transports: ["websocket"],
+  path: "/",
+});
+
+/** Fired when a new connection is established. */
+io.on("connection", async (socket) => {
+  console.log("open", {
+    id: socket.id,
+    readyState: socket.readyState,
+  });
+
+  socket.on("handshake", () => console.log("handshake"));
+
+  /** Fired when the client sends a message. */
+  socket.on("message", (data: string | Buffer) => {
+    // 편의상 echo
+    console.log("message", data);
+    socket.send(data);
+  });
+
+  /** Fired when an error occurs. */
+  socket.on("error", (err: Error) => console.log("error", err));
+
+  /** Fired when the client is disconnected. */
+  socket.on("close", (reason: string) => console.log("close", reason));
+
+  // 명시적으로 ping 호출하는거 테스트
+  await setTimeout(1_000);
+  socket.send("ping", {
+    wsPreEncoded: "2probe",
+  });
+});
+
+/**
+ * Fired on the first request of the connection, before writing the response headers
+ * Arguments
+ * headers (Object): a hash of headers
+ * req (http.IncomingMessage): the request
+ */
+io.on("initial_headers", (headers, req) => {
+  console.log("initial_headers");
+});
+
+/**
+ * Fired on the all requests of the connection, before writing the response headers
+ * Arguments
+ * headers (Object): a hash of headers
+ * req (http.IncomingMessage): the request
+ */
+io.on("headers", (headers, req) => {
+  console.log("headers");
+});
+
+/**
+ * Fired when an error occurs when establishing the connection.
+ * Arguments
+ * error: an object with following properties:
+ * req (http.IncomingMessage): the request that was dropped
+ * code (Number): one of Server.errors
+ * message (string): one of Server.errorMessages
+ * context (Object): extra info about the error
+ */
+io.on("connection_error", (err, socket) => {
+  console.log("connection_error", err, socket);
+});
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`\x1B[96mlistening on localhost:${port} \x1B[39m`);
+});
